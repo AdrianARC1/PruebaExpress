9871baac5b5012c98d33f347fdde64687142aebe
Adrian <adrianramon2003@gmail.com>
2023-02-21 16:54:14 +0100

    Conexion clevercloud

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { createPool } = require('mysql2/promise')
 
 const pool = createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: 3306
+    host: process.env.CLOUD_HOST,
+    user: process.env.CLOUD_USER,
+    password: process.env.CLOUD_PASSWORD,
+    database: process.env.CLOUD_DATABASE,
+    port: process.env.CLOUD_PORT
 })
 
 module.exports=pool
